4c8db3d1ff74333f465fd535e8d647b6285fab7f
Yann Simon <yann.simon.fr@gmail.com>
2012-08-21 09:30:42 +0200

    specification of line parser

---
+++ test/models/LineParserSpec.scala
@@ -0,0 +1,40 @@
+package models
+
+import org.specs2.mutable._
+import play.api.libs.iteratee.{Iteratee, Enumerator}
+import models.GrowthStream._
+
+class LineParserSpec extends Specification {
+
+  "A source line" should {
+    "be parsed" in {
+      val input = Enumerator("690\t12.51\t-70.01")
+      val values = joinValues(input &> lineParser)
+      values mustEqual List(Some(Coordinate(12.51, -70.01)))
+    }
+  }
+
+  "An unparsable source line" should {
+    "not break the parsing" in {
+      val input = Enumerator("688\t-5.5\t10.3", "hello", "690\t12.51\t-70.01")
+      val values = joinValues(input &> lineParser)
+      values mustEqual List(Some(Coordinate(-5.5,10.3)), None, Some(Coordinate(12.51, -70.01)))
+    }
+  }
+
+  "An unparsable source line" should {
+    "be filtered out" in {
+      val input = Enumerator("688\t-5.5\t10.3", "hello", "690\t12.51\t-70.01")
+      val values = joinValues(input &> lineParser &> validCoordinate)
+      values mustEqual List(Coordinate(-5.5,10.3), Coordinate(12.51, -70.01))
+    }
+  }
+  /**
+   * @return list of enumerated values of type A
+   */
+  private def joinValues[A](values: Enumerator[A]): List[A] = {
+    val join = Iteratee.fold[A, List[A]](Nil)((list, el) => el :: list)
+    Iteratee.flatten(values |>> join).run[List[A]].value.get.reverse
+  }
+
+}
