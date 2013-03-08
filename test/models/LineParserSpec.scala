5d24f5aed2c9527975d56cfe125cd4f517632f2c
Yann Simon <yann.simon.fr@gmail.com>
2013-03-08 08:27:39 +0100

    unit test 'validCoordinate' enumeratee

--- test/models/LineParserSpec.scala
+++ test/models/LineParserSpec.scala
@@ -24,6 +24,14 @@ class LineParserSpec extends Specification {
     }
   }
 
+  "unvalid coordinates" should {
+    "be filtered out" in {
+      val input = Enumerator(Some(Coordinate(-5.5,10.3)), None, Some(Coordinate(12.51, -70.01)))
+      val values = joinValues(input &> validCoordinate)
+      values mustEqual List(Coordinate(-5.5,10.3), Coordinate(12.51, -70.01))
+    }
+  }
+
   "An unparsable source line" should {
     "be filtered out" in {
       val input = Enumerator("688\t-5.5\t10.3", "hello", "690\t12.51\t-70.01", "a\tb\tc")
@@ -31,6 +39,7 @@ class LineParserSpec extends Specification {
       values mustEqual List(Coordinate(-5.5,10.3), Coordinate(12.51, -70.01))
     }
   }
+
   /**
    * @return list of enumerated values of type A
    */
